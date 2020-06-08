2fd1c989294cf8a504a1b09ccc648caff9b65911
Ricardo Acuña <racunas1500@alumno.ipn.mx>
2020-06-08 04:04:20 -0500

    Se ordenó el código. Se creo una funcion algorimoBusqueda(). Se hizo que actualizara automáticamente la lista de contactos al buscar.

--- lib/prueba_harry.dart
+++ lib/prueba_harry.dart
@@ -21,162 +21,239 @@ class _PruebaHarry extends State<PruebaHarry> {
 
   final _formKey = GlobalKey<FormState>();
   final ControlerContacto = TextEditingController();
-  List contactos = [];
-  var doctores = [];
-  List resultadoBusqueda = [];
-  List _contactos = [];
-  bool cargarDatos = false;
+  List contactos;
+  var doctores;
+  List resultadoBusqueda;
+  bool datosCargados = false;
 
   void peticionDoctores() async {
     var url = 'https://divumclinic.com/opinion/app/obtenerDoctores.app.php';
-
     http.Response respuesta = await http.post(
       url
     );
-    
     var _respuesta = jsonDecode(respuesta.body);
-
     setState(() {
       doctores = _respuesta;
     });
-
   }
   
 
-  void ObtenerPermisoContacto() async{
+  void obtenerPermisoContacto() async{
+    //PEDIMOS PERMISO PARA ACCEDER A LOS CONTACTOS
     if (await Permission.contacts.request().isGranted) {
+      contactos = []; //INICIALIZAMOS LA LISTA QUE VAMOS A UTILIZAR
       await Contacts.streamContacts().forEach((contact) {
-        contactos.add({
-              "nombreContacto":contact.displayName,
-              "telefono":(contact.phones.length > 0) ? contact.phones.first.value : ""
-            });
-      });                
-      setState(() {
-        contactos.sort((a, b) => a["nombreContacto"].toString().compareTo(b["nombreContacto"].toString()));
-        cargarDatos = true;
+        //AGREAGAMOS TODOS Y CADA UNO DE LOS CONTACTOS DEL DISPOSITIVO A LA LISTA DE contactos, SÓLO SÍ TIENE UN NÚMERO DE TELÉFONO
+        if(contact.phones.length > 0){
+          contactos.add({
+            "nombreContacto":contact.displayName,
+            "telefono":contact.phones.first.value
+          });
+        }
+        
+      }).whenComplete((){
+        //CUANDO TERMINE DE AGREGAR LOS CONTACTOS, LOS ORDENAMOS POR ORDEN ALFABETICO
+        setState(() {
+          //contactos.sort((a, b) => a["nombreContacto"].toString().compareTo(b["nombreContacto"].toString()));
+          datosCargados = true; //LE DECIMOS AL SISTEMA QUE YA HEMOS CARGADO TODOS LOS DATOS DE LOS CONTACTOS
+        });
       });
     }
   }
 
+  Future <List> algoritmoBusqueda(String terminoBusqueda, List dondeBuscar) async{
+    var respuesta = dondeBuscar.where((contacto){
+      //print(contacto['nombreContacto'].toLowerCase());
+      return contacto['nombreContacto'].toString().toLowerCase().contains(terminoBusqueda.toLowerCase());
+    }).toList();
+    return respuesta;
+  }
+
+  void buscarContacto (String terminoBusqueda) async{
+
+    if(terminoBusqueda != ""){
+      resultadoBusqueda = await algoritmoBusqueda(terminoBusqueda, contactos);
+      Future.delayed(Duration(seconds: 1)).whenComplete((){
+        setState(() {
+          datosCargados = true;  
+        });
+      });
+    }
+    
+  } 
+
   @override
   void initState(){
     super.initState();
-    
-    ObtenerPermisoContacto();
+    obtenerPermisoContacto();
     peticionDoctores();
-
   }
-
-  void buscarContacto (String terminoBusqueda) {
-    //resultadoBusqueda = [];
-
-    setState((){
-      cargarDatos = false;
-    });
-
-    if(terminoBusqueda == ""){
-      contactos = _contactos;
-    } else {
-      contactos = _contactos;
-      contactos = contactos.where((contacto) => contacto['nombreContacto'].toLowerCase().contains(terminoBusqueda.toLowerCase())).toList();
-    }
-
-    setState((){
-      cargarDatos = true;
-    });
-
-    print(contactos);
-    
-  } 
 
 
   @override
   Widget build(BuildContext context) {
    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.dark.copyWith(
-        statusBarColor: Colors.black
+        statusBarColor: Color(0xFFdae292)
     ));
-    return Scaffold(
-        body: Stack(
-          children: <Widget>[
-            Container(
-              padding: EdgeInsets.only(
-                left: 20,
-                top: 15
-              ),
-              child: ListView.builder(
-                padding: EdgeInsets.only(top:100),
-                itemBuilder: (BuildContext context, int i){
-                  
-                  if(!cargarDatos){
-                    print(contactos.length);
-                    return Center(child: CircularProgressIndicator(),);
-                    
-                  } else {
-                    return CardContactos(
-                      contactos[i]['nombreContacto'], 
-                      contactos[i]['telefono'],
-                      doctores
-                    );
-                  }
-                  
-                      
-                },
-                itemCount: contactos.length,
-              ),
-            ),
-            Container(
-              padding: EdgeInsets.only( 
-                top: 40,
-                left: 23,
-                right: 23
-              ),
-              color: Color(0xFFdae292),
-              height: 90,
-              child: Form(
-                key: _formKey,
-                child: TextFormField(
-                  controller: ControlerContacto,
-                  textInputAction: TextInputAction.done,
-                  onFieldSubmitted: (respuesta){
-                    setState((){
-                      buscarContacto(respuesta);
-                    });
-                  },
-                  onChanged: (respuesta){
-                   // print(respuesta);
-                    
-                  },
-                  style: TextStyle(
-                    fontFamily: 'Jost',
-                    color: Color(0xFFB3B3B3),
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold
-                  ),
-                  decoration: InputDecoration(
-                    border: InputBorder.none,
-                    hintText: 'Busca el contacto',
-                    hintStyle: TextStyle(
-                      fontFamily: 'Jost',
-                      color: Color(0xFFB3B3B3),
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
-                    ),
-                    suffixIcon: IconButton(
-                      icon: Icon(
-                        FontAwesomeIcons.search,
-                        color: Color(0xFFB3B3B3)
-                        
-                      ), 
-                      onPressed: (){
 
-                      }
+    final inputBuscador = Container(
+      padding: EdgeInsets.only( 
+        left: 10,
+        right: 10
+      ),
+      color: Color(0xFFdae292),
+      child: TextField(
+          controller: ControlerContacto,
+          textInputAction: TextInputAction.done,
+          onChanged: (respuesta){
+            if(respuesta == ""){
+              setState(() {
+                resultadoBusqueda = null;
+                datosCargados = true;
+              });
+            } else {
+              setState((){
+                resultadoBusqueda = null;
+                datosCargados = false;
+                buscarContacto(respuesta);
+              });
+            }
+          },
+          style: TextStyle(
+            fontFamily: 'Jost',
+            color: Color(0xFFB3B3B3),
+            fontSize: 22,
+            fontWeight: FontWeight.bold
+          ),
+          decoration: InputDecoration(
+            border: InputBorder.none,
+            hintText: 'Busca un contacto aquí...',
+            hintStyle: TextStyle(
+              fontFamily: 'Jost',
+              color: Color(0xFFB3B3B3),
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        )
+    );
+
+    final listaContactos = ListView.builder(
+        padding: EdgeInsets.only(top:25, left: 15, right: 15),
+        itemBuilder: (BuildContext context, int i){
+          
+          if(!datosCargados){
+
+            return Center(
+              child: Container(
+                margin: EdgeInsets.only(top: 180),
+                child: Column(
+                  children: <Widget>[
+                    CircularProgressIndicator(),
+                    SizedBox(height: 20,),
+                    Text(
+                      "Buscando contactos...",
+                      style: TextStyle(
+                        fontFamily: 'Jost',
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            );
+            
+          } else {
+
+            //SÍ NO HAY NADA ESCRITO EN EL BUSCADOR, MOSTRAMOS TODOS LOS CONTACTOS DE  LA LISTA contactos
+            if(ControlerContacto.text == ""){
+              return CardContactos(
+                contactos[i]['nombreContacto'], 
+                contactos[i]['telefono'],
+                doctores
+              );
+            } else {
+              //SÍ HAY ALGO ESCRITO EN EL BUSCADOR, VAMOS A MOSTRAR LA LISTA resultadosBusqueda
+
+              //PRIMERO VALIDAMOS QUE HAYA RESULTADOS EN LA LISTA resultadosBusqueda, Y SÍ LO HAY, LO MOSTRAMOS
+              if(resultadoBusqueda != null && resultadoBusqueda.length >= 1){
+                return CardContactos(
+                  resultadoBusqueda[i]['nombreContacto'], 
+                  resultadoBusqueda[i]['telefono'],
+                  doctores
+                );
+
+              } else {
+                //SÍ NO HAY RESULTADOS DE BÚSQUEDA, MOSTRAMOS UN MENSAJE DE QUE NO EXISTE NINGÚN CONTACTO CON ESE NOMBRE
+                return Center(
+                  child: Container(
+                    margin: EdgeInsets.only(top: 180),
+                    child: Column(
+                      children: <Widget>[
+                        Icon(Icons.account_circle, size: 35, color: Colors.grey,),
+                        SizedBox(height: 20,),
+                        Text(
+                          "No existe el contacto ${ControlerContacto.text}",
+                          style: TextStyle(
+                            fontFamily: 'Jost',
+                          ),
+                        )
+                      ],
                     ),
                   ),
-                )
-                ),     
-            )
-          ],
-        )
+                );
+              }
+
+            }
+          }
+          
+              
+        },
+        itemCount: (datosCargados) ? 
+                      (resultadoBusqueda != null && ControlerContacto.text != "") ?
+                          (resultadoBusqueda.length >= 1) ? resultadoBusqueda.length : 1 
+                          :
+                          (contactos != null && contactos.length >= 1) ?
+                            contactos.length :
+                            1
+                    :
+                      1
+      );
+
+    final botonActualizarContactos = IconButton(
+      icon: Icon(Icons.cached, size: 30,color: Color(0xFFB3B3B3)),
+      onPressed: (){
+
+      },
+    );
+
+    final botonEliminarTextoBusqueda = IconButton(
+      icon: Icon(
+        FontAwesomeIcons.times,
+        color: Color(0xFFB3B3B3),
+        size: 28,
+      ), 
+      onPressed: (){
+        if(ControlerContacto.text != ""){
+          setState((){
+            resultadoBusqueda = null;
+            ControlerContacto.text = "";
+            datosCargados = true;
+          });
+        } 
+      }
+    );
+
+    return Scaffold(
+      appBar: AppBar(
+        title: inputBuscador,
+        backgroundColor: Color(0xFFdae292),
+        automaticallyImplyLeading: true,
+        actions: <Widget>[
+          botonEliminarTextoBusqueda
+          //botonActualizarContactos
+        ],
+      ),
+      body: listaContactos
     );
   }
 }
