d155f8177304c11cfb62a74b0b79453ca99ef1d1
Ricardo Acuña <racunas1500@alumno.ipn.mx>
2020-06-08 00:32:47 -0500

    Se quitó el código de prueba en el main

--- lib/main.dart
+++ lib/main.dart
@@ -10,84 +10,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Divum Encuestas',
       theme: ThemeData(
         primarySwatch: Colors.green,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: PruebaHarry(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
-
-  @override
-  State<StatefulWidget> createState() {
-    return _MyHomePageState();
-  }
-
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-  void _incrementCounter() {
-    /*setState(() {
-      
-    });*/
-
-    _counter++;
-
-    setState(() {
-      
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-
-    return Scaffold(
-
-      
-      appBar: AppBar(
-        backgroundColor: Color(0xFFdbe387),
-        title: Text(widget.title),
-
-      ),
-
-
-
-
-      body: Center(
-        
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-
-
-
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Text(
-          'Omar'),
-      ),   
-
-
-
     );
   }
 }
